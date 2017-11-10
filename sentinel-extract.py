fc94a2354ac6d59fb2f79a87b2441c83745ef8bd
Tony Cannistra <tony.cannistra@gmail.com>
2017-11-09 17:53:25 -0800

    unfinished skeleton of ingest script

---
+++ sentinel-extract.py
@@ -0,0 +1,33 @@
+'''
+
+sentinel-extract.py
+
+selects sentinel satellite scenes for (date, geometry) pairs. 
+
+Takes as input two files: metadata and geodb
+
+
+'''
+
+import argparse
+import pandas as pd
+from imgproc import *
+from lakedata import *
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='selects sentinel satellite scenes for (date, geometry) pairs.')
+    parser.add_argument(
+        'metadata', help='csv must contain \'date\' and \'geoid\'columns')
+    parser.add_argument(
+        'geodb', help='shapefile, must contain \'geoid\' polygon identifier.')
+    args = parser.parse_args()
+
+    metadata = pd.read_csv(args['metadata'])
+    #assert('contains' is in metadata.columns)
+    geodata  = gpd.read_file(args['geodb'])
+
+
+if __name__ == "__main__":
+    main()
